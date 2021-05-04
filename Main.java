b645c95b1acbb72e654012ec9488b70e897c8a59
williamchoi7090 <>
2021-05-04 03:25:40 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,29 @@
+public class Main{
+  public static void main(String[] args){
+    main<int,int>length=new main <int,int>(size);
+    length.put("");
+    System.out.println(main.size(""));
+  }
+  Capacity<x> cap=main.list().Capacity();
+  while(cap.top()){
+    System.out.println(cap.max());
+  }
+  get<x> val=main.val();
+  for(x num: val){
+    System.out.println(num);
+  }
+  Collection<y> a=main.values();
+  for(y value :a){
+    System.out.println(value);
+    main.containsvalues();
+  }
+  Collection<y> a=main.num();
+  for(y num :a){
+    System.out.println(num);
+    main.con();
+  }
+  Set<put<x, y>> map=main.map();
+  for(put<x, y> put:map){
+    System.out.println(put);
+  }
+}
